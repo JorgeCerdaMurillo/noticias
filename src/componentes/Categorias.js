02aa768935af1aef5c633816822e19503e7c5fbe
Jorge Cerda Murillo <90658690+JorgeCerdaMurillo@users.noreply.github.com>
2021-10-13 22:44:50 -0500

    Add files via upload

--- src/componentes/Categorias.js
+++ src/componentes/Categorias.js
@@ -5,14 +5,14 @@ const Categorias = ({setCategoria}) => {
         setCategoria(eve.target.value);
     }
     return (
-            <div className="btn-categorias">
-                    <button  className="btntec btn-primary" value="technology" onClick={botones} >TECNOLOGÍA</button>
-                    <button  className="btnneg btn-primary" value="business" onClick={botones}>NEGOCIOS</button>
-                    <button  className="btnent btn-primary" value="entertainment" onClick={botones}>ENTRETENIMIENTO</button>
-                    <button   className="btnge btn-primary" value="general" onClick={botones}>GENERAL</button>
-                    <button className="btnsa btn-primary" value="health" onClick={botones}>SALUD</button>
-                    <button className="btncs btn-primary" value="science" onClick={botones}>CIENCIA</button>
-                    <button  className="btnde btn-primary" value="sports" onClick={botones}>DEPORTES</button>
+            <div className="btn-categorias" class="navbar navbar-light justify-content-center"  >
+                    <button class="b" className="btntec btn-primary text-bold m-3" value="technology" onClick={botones} >TECNOLOGÍA</button>
+                    <button class="b" className="btnneg btn-primary text-bold m-3" value="business" onClick={botones}>NEGOCIOS</button>
+                    <button class="b" className="btnent btn-primary text-bold m-3" value="entertainment" onClick={botones}>ENTRETENIMIENTO</button>
+                    <button  class="b" className="btnge btn-primary text-bold m-3" value="general" onClick={botones}>GENERAL</button>
+                    <button class="b"className="btnsa btn-primary text-bold m-3" value="health" onClick={botones}>SALUD</button>
+                    <button class="b"className="btncs btn-primary text-bold m-3" value="science" onClick={botones}>CIENCIA</button>
+                    <button class="b" className="btnde btn-primary text-bold m-3" value="sports" onClick={botones}>DEPORTES</button>
             </div>
     )
     
